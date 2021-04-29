a410089fc1b763fd05a1a15c808c18689f100109
skwita <skwita.max@gmail.com>
2021-04-29 20:45:59 +0300

    optimization

--- game_of_life.py
+++ game_of_life.py
@@ -5,6 +5,10 @@ from pygame.locals import *
 from grid import Grid
 
 
+def main(cur_game):
+    cur_game.run(cur_game)
+
+
 class GameOfLife:
     def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10):
         self.width = width
@@ -15,19 +19,71 @@ class GameOfLife:
         self.screen_size = width, height
         self.screen = pygame.display.set_mode(self.screen_size)
 
-        self.cell_width = int(self.width / self.cell_size)
-        self.cell_height = int(self.height / self.cell_size)
+        self.num_cell_horizontal = int(self.width / self.cell_size)
+        self.num_cell_vertical = int(self.height / self.cell_size)
+
+    def run(self, cur_game):
+        pygame.init()
+        cur_grid = cur_game.create_grid()
+        pygame.display.set_caption('Game of Life')
+        self.screen.fill(pygame.Color((48, 48, 48)))
+
+        is_game_running = True
+        is_running = False
+        is_mouse_btn_pressed = False
+        is_clicked_alive = False
+
+        while is_game_running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    is_game_running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    x = int(x / self.cell_size)
+                    y = int(y / self.cell_size)
+                    cur_grid.change_cell(x, y)
+                    is_clicked_alive = cur_grid.is_alive(x, y)
+                    is_mouse_btn_pressed = True
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    is_mouse_btn_pressed = False
+
+                if event.type == pygame.KEYDOWN:
+                    keys = pygame.key.get_pressed()
+                    if keys[K_UP]:
+                        cur_grid = self.step(cur_grid)
+                    if keys[K_SPACE]:
+                        is_running = not is_running
+                    if keys[K_DELETE]:
+                        cur_grid.clear()
+                    if keys[K_r]:
+                        cur_grid.randomize()
+
+            if is_mouse_btn_pressed:
+                x, y = pygame.mouse.get_pos()
+                x = int(x / self.cell_size)
+                y = int(y / self.cell_size)
+                if not is_clicked_alive:
+                    cur_grid.kill(x, y)
+                else:
+                    cur_grid.resurrect(x, y)
+
+            if is_running:
+                cur_grid = self.step(cur_grid)
+
+            self.draw_grid(cur_grid)
+            pygame.display.flip()
+        pygame.quit()
 
     def draw_grid(self, cur_grid):
-        for x in range(int(self.width / self.cell_size)):
-            for y in range(int(self.height / self.cell_size)):
+        for x in range(self.num_cell_horizontal):
+            for y in range(self.num_cell_vertical):
                 if cur_grid.is_alive(x, y):
                     i = random.randint(-20, 20)
                     j = random.randint(-20, 20)
                     k = random.randint(-20, 20)
-                    # a, b, c = 235, 162, 52  # (orange)
                     a, b, c = 149, 52, 235  # (purple)
-                    # a, b, c = 57, 235, 20  # (green)
                     pygame.draw.rect(self.screen, (a + i, b + j, c + k),
                                      (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
                 else:
@@ -38,85 +94,27 @@ class GameOfLife:
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, 'black', (0, y), (self.width, y))
 
-    def run(self):
-        pygame.init()
-        cur_grid = game.create_grid()
-        pygame.display.set_caption('Game of Life')
-        self.screen.fill(pygame.Color((48, 48, 48)))
-
-        running = True
-        temp_run = False
-        is_pressed = False
-        is_alive = False
-
-        while running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    running = False
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    x, y = pygame.mouse.get_pos()
-                    x = int(x / self.cell_size)
-                    y = int(y / self.cell_size)
-                    cur_grid.change_cell(x, y)
-                    is_alive = cur_grid.is_alive(x, y)
-                    is_pressed = True
-
-                if event.type == pygame.MOUSEBUTTONUP:
-                    is_pressed = False
-
-                if event.type == pygame.KEYDOWN:
-                    keys = pygame.key.get_pressed()
-                    if keys[K_UP]:
-                        cur_grid = self.step(cur_grid)
-                    if keys[K_SPACE]:
-                        temp_run = not temp_run
-                    if keys[K_DELETE]:
-                        cur_grid.clear()
-                    if keys[K_r]:
-                        cur_grid.randomize()
-
-            if is_pressed:
-                x, y = pygame.mouse.get_pos()
-                x = int(x / self.cell_size)
-                y = int(y / self.cell_size)
-                if not is_alive:
-                    cur_grid.kill(x, y)
-                else:
-                    cur_grid.resurrect(x, y)
-
-            if temp_run:
-                cur_grid = self.step(cur_grid)
-
-            self.draw_grid(cur_grid)
-            pygame.display.flip()
-        pygame.quit()
-
-    def create_grid(self):
-        return Grid(self.width / self.cell_size, self.height / self.cell_size)
-
     def step(self, cur_grid):
         new_grid = self.create_grid()
-        for i in range(int(self.width / self.cell_size)):
-            for j in range(int(self.height / self.cell_size)):
+        for i in range(self.num_cell_horizontal):
+            for j in range(self.num_cell_vertical):
                 num = cur_grid.get_neighbours(i, j)
                 alive = cur_grid.is_alive(i, j)
                 if alive:
                     if 1 < num < 4:
                         new_grid.resurrect(i, j)
-                    else:
-                        new_grid.kill(i, j)
-                else:
-                    if num == 3:
-                        new_grid.resurrect(i, j)
-                    else:
-                        new_grid.kill(i, j)
+                        continue
+                    new_grid.kill(i, j)
+                if num == 3:
+                    new_grid.resurrect(i, j)
+                    continue
+                new_grid.kill(i, j)
         return new_grid
+
+    def create_grid(self):
+        return Grid(self.num_cell_horizontal, self.num_cell_vertical)
 
 
 if __name__ == '__main__':
-    # game = GameOfLife(1920, 1000, 10)
-    # game = GameOfLife(1000, 1000, 10)
-    game = GameOfLife(1000, 1000, 5)
-
-    game.run()
+    game = GameOfLife(1000, 1000, 10)
+    main(game)
--- grid.py
+++ grid.py
@@ -2,56 +2,46 @@ import random
 
 
 class Grid:
-    def __init__(self, num_horizontal, num_vertical):
+    def __init__(self, num_horizontal: int, num_vertical: int):
         self.grid = []
         for i in range(int(num_horizontal)):
             self.grid.append([])
             for j in range(int(num_vertical)):
-                self.grid[i].append(0)
+                self.grid[i].append(False)
+        self.length = len(self.grid)
+        self.length_in = len(self.grid[0])
 
-    def change_cell(self, x: int, y: int):
-        if self.grid[x][y] == 0:
-            self.resurrect(x, y)
-        else:
-            self.kill(x, y)
+    def change_cell(self, x: int, y: int) -> None:
+        self.grid[x][y] = not self.grid[x][y]
 
-    def resurrect(self, x: int, y: int):
-        self.grid[x][y] = 1
+    def resurrect(self, x: int, y: int) -> None:
+        self.grid[x][y] = True
 
-    def kill(self, x: int, y: int):
-        self.grid[x][y] = 0
+    def kill(self, x: int, y: int) -> None:
+        self.grid[x][y] = False
 
-    def print(self):
-        for i in range(len(self.grid)):
-            print(str(self.grid[i]))
-        print('\n')
+    def is_alive(self, x: int, y: int) -> bool:
+        return self.grid[x][y]
 
-    def is_alive(self, x, y):
-        return self.grid[x][y] == 1
-
-    def get_neighbours(self, x, y):
+    def get_neighbours(self, x: int, y: int) -> int:
         num = 0
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if len(self.grid) > x + i >= 0 and len(self.grid[0]) > y + j >= 0:
-                    if self.grid[x + i][y + j] is not None and self.is_alive(x + i, y + j):
-                        if i == 0 and j == 0:
-                            pass
-                        else:
-                            num += 1
+        num += 1 if self.length > x - 1 >= 0 and self.length_in > y - 1 >= 0 and self.grid[x - 1][y - 1] else 0
+        num += 1 if self.length > x - 1 >= 0 and self.length_in > y + 0 >= 0 and self.grid[x - 1][y + 0] else 0
+        num += 1 if self.length > x - 1 >= 0 and self.length_in > y + 1 >= 0 and self.grid[x - 1][y + 1] else 0
+        num += 1 if self.length > x + 0 >= 0 and self.length_in > y - 1 >= 0 and self.grid[x + 0][y - 1] else 0
+        num += 1 if self.length > x + 0 >= 0 and self.length_in > y + 1 >= 0 and self.grid[x + 0][y + 1] else 0
+        num += 1 if self.length > x + 1 >= 0 and self.length_in > y - 1 >= 0 and self.grid[x + 1][y - 1] else 0
+        num += 1 if self.length > x + 1 >= 0 and self.length_in > y + 0 >= 0 and self.grid[x + 1][y + 0] else 0
+        num += 1 if self.length > x + 1 >= 0 and self.length_in > y + 1 >= 0 and self.grid[x + 1][y + 1] else 0
         return num
 
-    def clear(self):
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
+    def clear(self) -> None:
+        for i in range(self.length):
+            for j in range(self.length_in):
                 self.kill(i, j)
 
-    def randomize(self):
-        for i in range(0, len(self.grid)):
-            for j in range(0, len(self.grid[0])):
+    def randomize(self) -> None:
+        for i in range(0, self.length):
+            for j in range(0, self.length_in):
                 if random.randint(0, 9) < 2:
-                    self.resurrect(i - 1, j - 1)
-
-
-
-
+                    self.resurrect(i, j)
