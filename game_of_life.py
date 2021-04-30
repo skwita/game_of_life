f566050951da94a8bb8bb101639ad36786b9bf14
skwita <skwita.max@gmail.com>
2021-04-30 22:36:16 +0300

    refactoring added presets

--- game_of_life.py
+++ game_of_life.py
@@ -5,16 +5,11 @@ from pygame.locals import *
 from grid import Grid
 
 
-def main(cur_game):
-    cur_game.run(cur_game)
-
-
 class GameOfLife:
-    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10):
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10):
         self.width = width
         self.height = height
         self.cell_size = cell_size
-        self.speed = speed
 
         self.screen_size = width, height
         self.screen = pygame.display.set_mode(self.screen_size)
@@ -22,26 +17,23 @@ class GameOfLife:
         self.num_cell_horizontal = int(self.width / self.cell_size)
         self.num_cell_vertical = int(self.height / self.cell_size)
 
-    def run(self, cur_game):
+    def run(self):
         pygame.init()
-        cur_grid = cur_game.create_grid()
+        cur_grid = game.create_grid()
         pygame.display.set_caption('Game of Life')
         self.screen.fill(pygame.Color((48, 48, 48)))
 
-        is_game_running = True
         is_running = False
         is_mouse_btn_pressed = False
         is_clicked_alive = False
 
-        while is_game_running:
+        while 1:
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    is_game_running = False
+                    pygame.quit()
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    x, y = pygame.mouse.get_pos()
-                    x = int(x / self.cell_size)
-                    y = int(y / self.cell_size)
+                    x, y = [int(coord / self.cell_size) for coord in pygame.mouse.get_pos()]
                     cur_grid.change_cell(x, y)
                     is_clicked_alive = cur_grid.is_alive(x, y)
                     is_mouse_btn_pressed = True
@@ -59,11 +51,13 @@ class GameOfLife:
                         cur_grid.clear()
                     if keys[K_r]:
                         cur_grid.randomize()
+                    if keys[K_s]:
+                        cur_grid.save()
+                    if keys[K_l]:
+                        cur_grid = cur_grid.load()
 
             if is_mouse_btn_pressed:
-                x, y = pygame.mouse.get_pos()
-                x = int(x / self.cell_size)
-                y = int(y / self.cell_size)
+                x, y = [int(coord / self.cell_size) for coord in pygame.mouse.get_pos()]
                 if not is_clicked_alive:
                     cur_grid.kill(x, y)
                 else:
@@ -74,17 +68,14 @@ class GameOfLife:
 
             self.draw_grid(cur_grid)
             pygame.display.flip()
-        pygame.quit()
 
     def draw_grid(self, cur_grid):
         for x in range(self.num_cell_horizontal):
             for y in range(self.num_cell_vertical):
                 if cur_grid.is_alive(x, y):
-                    i = random.randint(-20, 20)
-                    j = random.randint(-20, 20)
-                    k = random.randint(-20, 20)
-                    a, b, c = 149, 52, 235  # (purple)
-                    pygame.draw.rect(self.screen, (a + i, b + j, c + k),
+                    d_r, d_g, d_b = [random.randint(-20, 20) for _ in range(3)]
+                    r, g, b = 149, 52, 235  # (purple)
+                    pygame.draw.rect(self.screen, (r + d_r, g + d_g, b + d_b),
                                      (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
                 else:
                     pygame.draw.rect(self.screen, (48, 48, 48),
@@ -112,9 +103,9 @@ class GameOfLife:
         return new_grid
 
     def create_grid(self):
-        return Grid(self.num_cell_horizontal, self.num_cell_vertical)
+        return Grid(self.num_cell_horizontal, self.num_cell_vertical, 0)
 
 
 if __name__ == '__main__':
     game = GameOfLife(1000, 1000, 10)
-    main(game)
+    game.run()
--- grid.py
+++ grid.py
@@ -1,8 +1,10 @@
+import glob
 import random
+import time
 
 
 class Grid:
-    def __init__(self, num_horizontal: int, num_vertical: int):
+    def __init__(self, num_horizontal: int, num_vertical: int, cur_num_saves: int):
         self.grid = []
         for i in range(int(num_horizontal)):
             self.grid.append([])
@@ -10,6 +12,7 @@ class Grid:
                 self.grid[i].append(False)
         self.length = len(self.grid)
         self.length_in = len(self.grid[0])
+        self.cur_num_saves = cur_num_saves
 
     def change_cell(self, x: int, y: int) -> None:
         self.grid[x][y] = not self.grid[x][y]
@@ -45,3 +48,26 @@ class Grid:
             for j in range(0, self.length_in):
                 if random.randint(0, 9) < 2:
                     self.resurrect(i, j)
+
+    def save(self) -> None:
+        file = open(f"s{time.time()}.txt", 'w')
+        file.write(f"{self.length} {self.length_in} \n")
+        for i in range(self.length):
+            for j in range(self.length_in):
+                file.write(f"{1 if self.grid[i][j] else 0} ")
+            file.write(" \n")
+        file.close()
+
+    def load(self):
+        file = open(glob.glob("presets/s*.txt")[self.cur_num_saves], 'r')
+        print(glob.glob("presets/s*.txt")[self.cur_num_saves].split("\\")[1])
+        self.cur_num_saves = 0 if self.cur_num_saves == len(glob.glob("presets/s*.txt")) - 1 else self.cur_num_saves + 1
+        length, length_in, *_ = file.readline().split(" ")
+        new_grid = Grid(length, length_in, self.cur_num_saves)
+        for x in range(self.length):
+            line = file.readline().split(" ")
+            for index, cell in enumerate(line):
+                if cell == "1":
+                    new_grid.resurrect(x, index)
+        file.close()
+        return new_grid
